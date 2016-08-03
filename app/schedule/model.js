a8e003f50f485c150d2eddfe3cf0d4ce0360c5ae
shkherad <shkherad@bu.edu>
2016-08-03 15:54:58 -0400

    schedule model adj no inverses

--- app/schedule/model.js
+++ app/schedule/model.js
@@ -1,7 +1,16 @@
 import DS from 'ember-data';
 import { belongsTo } from 'ember-data/relationships';
 
+// export default DS.Model.extend({
+//   profile: DS.belongsTo('profile', { inverse: 'schedules' }),
+//   recipe: DS.belongsTo('recipe', { inverse: 'schedules' }),
+//   complete: DS.attr('boolean'),
+//   favorite: DS.attr('boolean'),
+// });
+
 export default DS.Model.extend({
-  profile: DS.belongsTo('profile', { inverse: 'schedules' }),
-  recipe: DS.belongsTo('recipe', { inverse: 'schedules' }),
+  profile: DS.belongsTo('profile'),
+  recipe: DS.belongsTo('recipe'),
+  complete: DS.attr('boolean'),
+  favorite: DS.attr('boolean'),
 });
